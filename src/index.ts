9b6c3867476bc44ede6234fdf47c08d36f84286f
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2020-11-28 23:44:50 +0000

    Create version file on first launch

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import {config} from 'dotenv';
-import {loop} from './helpers/functions';
+import { existsSync } from 'fs';
+import { versionFile } from './helpers/constants';
+import {getLatestVersion, loop} from './helpers/functions';
+import { writeVersionToFile } from './helpers/utils';
 
 (async () => {
   config(); // Load .env file
   console.log('Minecraft version bot started');
+
+  if (!existsSync(versionFile)) {
+    console.log('Version file not found! Creating from latest...');
+    writeVersionToFile(await getLatestVersion(), versionFile);
+  }
+
   await loop();
 })();
